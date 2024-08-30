fb7912959cc5035ba297e18312d2c0a5ba04034a
arvrao <arvindrao.759@gmail.com>
2024-08-30 21:01:44 +0530

    Added routes for get and post cache api

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"lru-cache-app/cache"
+	"lru-cache-app/handlers"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,10 +11,15 @@ func main() {
 	// Initialize Fiber app
 	app := fiber.New()
 
+	// Initialize LRU cache with a capacity of 1024
+	cache := cache.NewLRUCache(1024)
+
 	// Register routes
-	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("hello")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to LRU Cache Application. Hope you'll enjoy it!")
 	})
+	app.Get("/cache/:key", handlers.GetCacheValue(cache))
+	app.Post("/cache", handlers.SetCacheValue(cache))
 
 	// Start the server on port 3000
 	app.Listen(":3000")
